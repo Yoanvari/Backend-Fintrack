52e28e4e5f5415a824878aee08a7edc22c1c214d
Yoanvari <yoanvari123@gmail.com>
2025-04-21 22:21:11 +0700

    Add migration files for budget details tables

---
+++ database/migrations/2025_04_21_151803_crate_budget_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budget_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('budget_id');
+            $table->unsignedBigInteger('transaction_id');
+            $table->decimal('amount');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+            $table->softDeletes('deleted_at');
+
+            // Foreign key constraints
+            $table->foreign('budget_id')->references('id')->on('budgets')->onDelete('cascade');
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budget_details');
+    }
+};
