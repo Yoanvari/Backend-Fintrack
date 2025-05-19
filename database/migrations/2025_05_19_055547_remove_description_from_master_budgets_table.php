cfdd38cbacd60217cdf0aa2988326e22489eb920
Muzzarino2811 <lordmuzzar2811@gmail.com>
2025-05-19 13:07:43 +0700

    Hapus bekas unit testing

--- database/migrations/2025_05_19_055547_remove_description_from_master_budgets_table.php
+++ database/migrations/2025_05_19_055547_remove_description_from_master_budgets_table.php
@@ -4,22 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDescriptionToMasterBudgetsTable extends Migration
+return new class extends Migration
 {
-    
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::table('master_budgets', function (Blueprint $table) {
-            $table->text('description')->nullable()->after('some_column'); 
-            
+            //
         });
     }
 
-
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::table('master_budgets', function (Blueprint $table) {
-            $table->dropColumn('description');
+            //
         });
     }
-}
+};
