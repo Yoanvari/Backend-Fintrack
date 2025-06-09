ff44e52e3f095bb7ec7f851fe20be15ffeba3759
Yoanvari <yoanvari123@gmail.com>
2025-06-09 20:45:20 +0700

    Add RakController and RakResource for budget detail management; implement methods for retrieving, updating, and deleting budget details by branch.

---
+++ app/Http/Resources/RakResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\BudgetDetailResource;
+
+class RakResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'period' => $this->period->format('Y-m-d'),
+            'submission_date' => $this->submission_date->format('Y-m-d H:i:s'),
+            'status' => $this->status,
+            'revision_note' => $this->revision_note,
+            'total_amount' => $this->detail_sum_amount,
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'branch' => [
+                'id' => $this->branch->id,
+                'name' => $this->branch->branch_name,
+            ],
+            'detail' => BudgetDetailResource::collection($this->whenLoaded('detail')),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
