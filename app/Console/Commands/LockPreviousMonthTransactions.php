12a2b9df5d9356c0e9903dbfc695660ec29e16f3
Yoanvari <yoanvari123@gmail.com>
2025-06-11 18:33:36 +0700

    Add LockPreviousMonthTransactions command to lock previous month's transactions and schedule it monthly

---
+++ app/Console/Commands/LockPreviousMonthTransactions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class LockPreviousMonthTransactions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lock:transactions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $lastMonth = now()->subMonth();
+        
+        \DB::table('transactions')
+            ->whereMonth('transaction_date', $lastMonth->month)
+            ->whereYear('transaction_date', $lastMonth->year)
+            ->update(['is_locked' => 1]);
+
+        $this->info('Transaksi bulan sebelumnya berhasil dikunci.');
+    }
+}
